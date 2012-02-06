b9cd1351f4bfc0165292ed7ea653e43fa043e728
Tsuyoshi Okumura <qkrhn081@gmail.com>
2012-02-06 10:24:43 +0900

    add source.

---
+++ getexam.rb
@@ -0,0 +1,181 @@
+#!/usr/bin/ruby
+
+require 'uri'
+require 'net/http'
+require 'rubygems'
+require 'xml/libxml'
+#require 'progressbar'
+require 'date'
+require 'csv'
+
+Net::HTTP.version_1_1
+
+HOST = "localhost"
+PORT = "8000"
+USER = "ormaster"
+PASSWD = "ormaster123"
+
+CONTENT_TYPE = "application/xml"
+$body = ""
+$date = ""
+$csvName = "./examList"
+
+def setBody(id, dt, sq, insNum)
+    $body = <<-EOS
+      <data>
+	<record>
+	  <record name="medicalgetreq">
+	    <string name="Patient_ID">#{id}</string>
+	    <string name="Perform_Date">#{dt}</string>
+	    <record name="Medical_Information">
+	      <string name="Department_Code">#{$dipCode}</string>
+	      <string name="Sequential_Number">#{sq}</string>
+	      <string name="Insurance_Combination_Number">#{insNum}</string>
+	      <record name="HealthInsurance_Information">
+		<string name="InsuranceProvider_Class"></string>
+		<string name="InsuranceProvider_WholeName"></string>
+		<string name="InsuranceProvider_Number"></string>
+		<string name="HealthInsuredPerson_Symbol"></string>
+		<string name="HealthInsuredPerson_Number"></string>
+		<array name="PublicInsurance_Information">
+		  <record>
+		    <string name="PublicInsurance_Class"></string>
+		    <string name="PublicInsurance_Name"></string>
+		    <string name="PublicInsurer_Number"></string>
+		    <string name="PublicInsuredPerson_Number"></string>
+		  </record>
+		</array>
+	      </record>
+	    </record>
+	  </record>
+	</record>
+      </data>
+    EOS
+end
+
+def requestApi(classNo, reqxml)
+    req = Net::HTTP::Post.new("/api01r/medicalget?class=0" + classNo)
+    req.content_length = reqxml.size
+    req.content_type = CONTENT_TYPE
+    req.body = reqxml
+    req.basic_auth(USER, PASSWD)
+
+    system("date")
+    Net::HTTP.start(HOST, PORT) {|http|
+    res = http.request(req)
+    system("date")
+
+    @filename = './res/' + classNo + "_" + $date +  '.xml'
+
+    File.open(@filename, 'w') {|f|
+      f.write res.body
+    }
+
+    #print("xml start \r\n")
+    @uri = open(@filename) do |f|
+      f.read
+    end
+    }
+
+    $xml = LibXML::XML::Document.string(@uri)
+    $xml.save(@filename, :indent => true, :encoding => LibXML::XML::Encoding::UTF_8)
+
+    #puts "requestApi end!"
+end
+
+def makeExamCsv()
+    i = 0
+    j = 0
+    csv = ""
+    listH = [""]
+    listF = [""]
+
+    csv.concat("### " + $date + " ###" + "\r\n")
+
+    $xml.root.find( '/data/record/record/array/record/array/record' ).each do |elem|
+      
+      elem.find('string').each do |node|
+	if (node.content != "") then
+	  listH[i] = node.content
+	  i = i + 1
+	end
+      end 
+      if (listH.size == 6)
+	listH[5].concat("\r\n")
+	csv.concat(listH.join(','))
+      end
+      listH.clear
+      i = 0
+
+      elem.find('array').each do |node|
+	node.find('record').each do |node2|
+	  node2.find('string').each do |node3|
+	    if (node3.content != "")
+	      listF[j] = node3.content
+	    else
+	      break
+	    end
+	    j = j + 1
+	  end
+
+	  if (listF.size == 3)
+	    listF[2].concat("\r\n")
+	    csv.concat(listF.join(','))
+	  end
+
+	  listF.clear
+	  j = 0
+	end
+      end
+    end
+
+    #File.open($csvName + "_" + $ptIdd + ".csv", 'a') {|f|
+    File.open($csvName + ".csv", 'a') {|f|
+      f.write csv
+    }
+end
+
+# class :01 受信履歴取得
+#       :02 日別診療情報取得
+#       :03 月別診療情報取得
+#
+#1.患者番号       Patient_ID            (REQUIRED)
+#2.診療年月(日)   Perform_Date          (IMPLIED)
+#3.診療科コード   Department_Code       (REQUIRED)
+#4.連番           Sequential_Number     (IMPLIED)
+#
+#  REQUIRED : 必須   IMPLIED : 任意
+
+#puts "診療科コードを入力してください(例:01)"
+#$dipCode = gets.chomp
+$dipCode = "01"
+puts "患者番号の桁数を入力して下さい"
+#$idLen = gets.chomp
+puts "10"
+$idLen = "10"
+
+if File.exist?($csvName)
+  File.delete($csvName)
+end
+
+CSV.open("test.csv", "r") do |row|
+  $ptId = row[0]
+  $date = row[1].to_s
+  $date = $date[0..3] + "-" + $date[4..5] + "-" + $date[6..7]
+  $insNum = row[2]
+  $seq = row[3]
+  setBody($ptId, $date, $seq, $insNum)
+  requestApi("2", $body)
+  makeExamCsv()
+end
+
+=begin
+  p $ptId
+  p $date
+  p $insNum
+  p $seq
+=end
+
+puts "finish"
+
+
---
+++ makeConvertList.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+CSVFILE=/tmp/jyurrk.csv
+
+#最終来院日で患者を絞る
+
+#最終来院日を入力してもらう
+echo '取得するデータの一番古い最終来院日を入力してください。'
+read LASTDATE
+
+#取得する診療データの範囲を指定
+echo '取得する診療データの開始日を入力してください。'
+read STARTDATE
+echo '取得する診療データの終了日を入力してください。'
+read ENDDATE
+
+echo '患者数の取得中です・・・'
+#対象患者の患者番号を取得
+PTNUMS=`psql -q -t -A -U orca -d orca <<_EOF
+SELECT trim(ptnum) FROM tbl_srykarrk s,tbl_ptnum n WHERE s.ptid=n.ptid and lastymd >= '$LASTDATE' ORDER BY n.ptid
+_EOF`
+#
+
+#患者番号の配列を作り、一人一人の診察日を調べる
+PTNUM_ARRY=($PTNUMS)
+PTNUM_COUNT=${#PTNUM_ARRY[@]}
+#対象患者数報告
+echo "対象となる患者数は、${PTNUM_COUNT}人です。"
+
+
+echo '受信履歴データの取得中です・・・'
+JYURRK_ARRY=()
+for TARGET in ${PTNUM_ARRY[*]}
+do
+    JYURRKS=`psql -q -t -A -d orca -F, <<_EOF
+    SELECT trim(n.ptnum),j.sryymd,j.hkncombinum,j.rennum FROM tbl_jyurrk j,tbl_ptnum n WHERE j.ptid=n.ptid and n.ptnum = '$TARGET' and j.edanum = 1 and j.sryymd >= '$STARTDATE' and j.sryymd <= '$ENDDATE' ORDER BY j.sryymd
+_EOF`
+
+JYURRK_ARRY=(${JYURRK_ARRY[@]} $JYURRKS)
+done
+
+JYURRK_COUNT=${#JYURRK_ARRY[@]}
+#受信履歴データ数報告
+echo "受信履歴は、${JYURRK_COUNT}件です。"
+
+#CSVファイル出力
+#CSVファイルの構造
+# 患者番号,診療年月日(20111203),保険組合せ番号(0001),連番
+
+if [ -e $CSVFILE ]; then
+    rm $CSVFILE
+fi
+
+for (( i = 0; i < ${#JYURRK_ARRY[@]}; ++i))
+do
+    echo ${JYURRK_ARRY[$i]} >> $CSVFILE
+done
+exit
